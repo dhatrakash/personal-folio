9a15b3461c75a1d5f95a131d5e9c4c11a026cb28
Aishwarya Dhatrak <140381868+dhatrakash@users.noreply.github.com>
2024-03-14 22:52:35 +0530

    Add files via upload

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hello, I'm Aishwarya</h1>
+        <p className={styles.description}>
+          Full-Stack developer experienced in MERN
+          Stack. Reach out if you'd like to learn more!
+        </p>
+        <a href="mailto:myemail@email.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
+        <a href="#" className={styles.contactBtn}>
+          Resume
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/heroImage.png")}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
